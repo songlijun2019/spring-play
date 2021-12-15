9ea069970a2b52b131d0b214fbcec0408654c1cd
宋丽俊 <songlijun@dobot.cc>
2021-12-15 12:02:32 +0800

    上传flyway配置代码

---
+++ spring-play-mvn/src/main/java/com/slj/springplaymvn/SpringPlayMvnApplication.java
@@ -0,0 +1,14 @@
+package com.slj.springplaymvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlayMvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlayMvnApplication.class, args);
+        System.out.println("hello");
+    }
+
+}
